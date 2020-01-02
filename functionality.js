114d38ccbad59ecbaa2213a476aeb0c1d2158e57
hnmichaels <57650424+hnmichaels@users.noreply.github.com>
2020-01-02 11:55:10 -0600

    Updates all around

--- functionality.js
+++ functionality.js
@@ -1,14 +1,17 @@
-function openSection(att, sectionName) {
-	var i, tabcontent, tablinks;
-	tabcontent = document.getElementsBySectionName("tabcontent");
-	for (i = 0; i < tabcontent.length; i++) {
-		tabcontent[I].style.display = "none";
-		}
-	tablinks = document.getElementsBySectionName("tablinks");
-	for (i = 0; i < tablinks.length; i++) {
-		tablinks[I].sectionName = tablinks[I].sectionName.replace(" active", "");
-	}
-	document.getElementById(sectionName).style.display = "block";
-	att.currentTarget.sectionName += " active";
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
 }
 
